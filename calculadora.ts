d9ee8376fcd6428c71a246d2399426e514828d9e
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-09 11:04:52 -0300

    Add files via upload

---
+++ calculadora.ts
@@ -0,0 +1,47 @@
+class Calculadora{
+
+    static Somar(x: number, y: number): number{
+        return x+y;
+    }
+
+    static Subtrair(x: number, y:number): number{
+        return x-y;
+    }
+
+    static Dividir(x:number, y:number): number{
+        return x/y; 
+    }
+    
+    static Multiplicar(x: number, y: number): number{
+        return x*y;
+    }
+
+    static Potencia(x: number, y: number): number{
+        return Math.pow(x,y);
+    }
+
+    static Tabuada(x: number): number[]{
+        let values: number[] = [];
+        for (let index = 0; index <=10; index++) {
+            values.push(x*(index+1));
+        }
+        return values;
+    }
+    
+    static Par(x: number): boolean{
+        return (x%2==0);
+    }
+
+    static Impar(x: number): boolean{
+        return (x%2!=0);
+    }
+
+    static Positivo(x: number): boolean{
+        return (x>=0);
+    }
+
+    static Negativo(x: number): boolean{
+        return (x<0);
+    }
+}
+
